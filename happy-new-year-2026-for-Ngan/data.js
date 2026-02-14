2db2131128ac20d6182b208b1a65ed37ad0a03f2
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2026-02-14 08:10:55 +0700

    Update data.js

--- happy-new-year-2026-for-Ngan/data.js
+++ happy-new-year-2026-for-Ngan/data.js
@@ -3,12 +3,12 @@ const data_name = [
         ma_ten: "XXX",
         name: "XXX",
         message: "Hello, I'm Công",
-        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+        img: "https://drive.google.com/file/d/1J8CKrl_QychIuf2zVCRjxVhoXNupcYYg/view?usp=drive_link"
     },
     {
         ma_ten: "XXX",
         name: "XXX",
         message: "Hello, I'm Thanh",
-        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+        img: "https://drive.google.com/file/d/1J8CKrl_QychIuf2zVCRjxVhoXNupcYYg/view?usp=drive_link"
     }
 ]
